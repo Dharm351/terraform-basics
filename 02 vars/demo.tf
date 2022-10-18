e4ea3a47ef9f3ccdbe4032425097014a90165965
dharm351 <dharmendrasingh351@gmail.com>
2022-10-19 01:30:44 +0530

    dchange

--- 02 vars/demo.tf
+++ 02 vars/demo.tf
@@ -6,6 +6,9 @@ output "sample-op" {
     value = var.sample
 }
 
-# output "sample-op1" {
-#   value ${var.sample}
-#}
+output "sample-op1" {
+    value = "value is ${var.sample}"
+}
+
+# A variable can be accessed with or without ${} only if the variable is called.
+# But incase if it is needed to add with some other strings then we need to use double quotes.
