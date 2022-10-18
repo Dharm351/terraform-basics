0936ff6d6d471ef932948c29ba2e858855c6bd33
dharm351 <dharmendrasingh351@gmail.com>
2022-10-19 02:04:21 +0530

    d change

--- 02 vars/demo.tf
+++ 02 vars/demo.tf
@@ -28,5 +28,5 @@ variable "list" {
 # Above list can show us that we can put any type of data in one variable defination. 
 
 output "list-op" {
-  value = "Welcome to ${var.list[3]} learning and this contain ${var.list[0]} and the learner is ${var.list[6]  var.list[7]}"
+  value = "Welcome to ${var.list[3]} learning and this contain ${var.list[0]} and the learner is $${var.list[6] var.list[7]}"
 } 
