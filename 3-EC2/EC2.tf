6ce31a5a1abfd1e40434df46b59b47db854e552e
dharm <dharmendrasingh351@gmail.com>
2022-11-05 15:02:21 +0530

    2nd change

--- 3-EC2/EC2.tf
+++ 3-EC2/EC2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo trafrm" {
+resource "aws_instance" "demo_trafrm" {
   ami           = "ami-06f7b3cf9a1bd9bd2"
   instance_type = "t2.micro"
 
