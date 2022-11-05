0c092e0adc3acc958e19e0ecbc3d873f0036b9e8
dharm <dharmendrasingh351@gmail.com>
2022-11-05 15:34:39 +0530

    2nd change

--- 3-EC2/EC2.tf
+++ 3-EC2/EC2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "demo_trafrm" {
   ami           = "ami-06f7b3cf9a1bd9bd2"
   instance_type = "t2.micro"
   tags   = {
-    Name = "1 using trafrm"
+    Name = "1st using trafrm"
   }
 }
 
